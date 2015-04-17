10107bfd8d8815c7e5db7fbba6cddfa4816c7af0
Andrew Steele <andrew@andrewsteele.me.uk>
2015-04-17 10:49:56 +0100

    Added GPL statement to top of each file

--- stadiums.py
+++ stadiums.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+#  This file is part of OpenSoccerManager-Editor.
+#
+#  OpenSoccerManager-Editor is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+#  OpenSoccerManager-Editor is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#  Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  OpenSoccerManager-Editor.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from gi.repository import Gtk
 from gi.repository import Gdk
 import re
@@ -163,23 +179,49 @@ class Attributes(Gtk.Grid):
         commonframe = widgets.CommonFrame("Attributes")
         self.attach(commonframe, 0, 1, 3, 1)
 
+        grid2 = Gtk.Grid()
+        grid2.set_row_spacing(5)
+        grid2.set_column_spacing(5)
+        commonframe.insert(grid2)
+
         scrolledwindow = Gtk.ScrolledWindow()
+        scrolledwindow.set_size_request(-1, 120)
         scrolledwindow.set_policy(Gtk.PolicyType.NEVER,
                                   Gtk.PolicyType.AUTOMATIC)
-        commonframe.insert(scrolledwindow)
+        grid2.attach(scrolledwindow, 0, 0, 1, 1)
 
         self.liststoreAttributes = Gtk.ListStore(int, str, str)
 
-        treeview = Gtk.TreeView()
-        treeview.set_hexpand(True)
-        treeview.set_model(self.liststoreAttributes)
-        treeview.set_enable_search(False)
-        treeview.set_search_column(-1)
-        scrolledwindow.add(treeview)
+        self.treeview = Gtk.TreeView()
+        self.treeview.set_hexpand(True)
+        self.treeview.set_model(self.liststoreAttributes)
+        self.treeview.set_enable_search(False)
+        self.treeview.set_search_column(-1)
+        #self.treeview.connect("row-activated", self.attribute_activated)
+        self.treeselectionAttribute = self.treeview.get_selection()
+        #self.treeselectionAttribute.connect("changed", self.attribute_changed)
+        scrolledwindow.add(self.treeview)
         treeviewcolumn = widgets.TreeViewColumn(title="Year", column=1)
-        treeview.append_column(treeviewcolumn)
+        self.treeview.append_column(treeviewcolumn)
         treeviewcolumn = widgets.TreeViewColumn(title="Capacity", column=2)
-        treeview.append_column(treeviewcolumn)
+        self.treeview.append_column(treeviewcolumn)
+
+        buttonbox = Gtk.ButtonBox()
+        buttonbox.set_spacing(5)
+        buttonbox.set_layout(Gtk.ButtonBoxStyle.START)
+        buttonbox.set_orientation(Gtk.Orientation.VERTICAL)
+        buttonAdd = Gtk.Button.new_from_icon_name("gtk-add", Gtk.IconSize.BUTTON)
+        #buttonAdd.connect("clicked", self.add_attribute)
+        buttonbox.add(buttonAdd)
+        self.buttonEdit = Gtk.Button.new_from_icon_name("gtk-edit", Gtk.IconSize.BUTTON)
+        self.buttonEdit.set_sensitive(False)
+        #self.buttonEdit.connect("clicked", self.edit_attribute)
+        buttonbox.add(self.buttonEdit)
+        self.buttonRemove = Gtk.Button.new_from_icon_name("gtk-remove", Gtk.IconSize.BUTTON)
+        self.buttonRemove.set_sensitive(False)
+        #self.buttonRemove.connect("clicked", self.remove_attribute)
+        buttonbox.add(self.buttonRemove)
+        grid2.attach(buttonbox, 1, 0, 1, 1)
 
     def populate_attributes(self):
         '''
